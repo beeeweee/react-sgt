6283ffc38ff996acccf53c6d5d560710ea6e06d9
beeeweee <billdarnalljr@gmail.com>
2019-02-08 15:44:26 -0800

    created table with class and functional components in react

---
+++ src/data/get_all_students.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        id: 1,
+        name: 'Steve B.',
+        course: 'Math',
+        grade: 69
+    },
+    {
+
+        id: 2,
+        name: 'Chris C.',
+        course: 'Science',
+        grade: 75  
+    },
+    {
+        id: 3,
+        name: 'Bill D.',
+        course: 'Coding',
+        grade: 100  
+    },
+    {
+        id: 4,
+        name: 'Scott B.',
+        course: 'Diet Coke',
+        grade: 100  
+    },
+    {
+        id: 5,
+        name: 'Water T.',
+        course: 'Ice',
+        grade: 88  
+    },
+];
